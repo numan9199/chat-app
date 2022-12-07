5cc966fea11ce292cecaa02bf59b98748f83d6f8
numan9199 <6210110176@psu.ac.th>
2022-12-07 14:32:57 +0700

    edit

--- lib/chat/body.dart
+++ lib/chat/body.dart
@@ -1,7 +1,5 @@
 import 'package:app/infosample/Chat.dart';
-import 'package:app/chat/chatcard.dart';
 import 'package:app/message/messageScreen.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class body extends StatefulWidget {
@@ -78,5 +76,67 @@ class _bodyState extends State<body> {
             ],
           )),
     ]);
+  }
+}
+
+class chatcard extends StatelessWidget {
+  const chatcard({
+    Key? key,
+    required this.chat,
+    required this.press,
+  }) : super(key: key);
+
+  final Chat chat;
+  final VoidCallback press;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: press,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5.75),
+        child: Row(
+          children: [
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage(chat.image),
+                ),
+                if (chat.isActive)
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: Container(
+                      height: 16,
+                      width: 16,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 3)),
+                    ),
+                  )
+              ],
+            ),
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    chat.name,
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Text(chat.lastMessage)
+                ],
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
   }
 }
